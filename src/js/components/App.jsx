572a48fba59060b636e08e5d8c2d36ba0bf56f69
Chris Gilroy <cdgilroy24@gmail.com>
2023-05-21 16:19:12 +0100

    feat: bootstrap a React app

---
+++ src/js/components/App.jsx
@@ -0,0 +1,3 @@
+export const App = () => {
+  return <h1>Hello, I&apos;m a React component!</h1>;
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
+import { createRoot } from "react-dom/client";
+import { App } from "./components/App";
 
-console.log('App Ready');
+const root = createRoot(document.getElementById("exercise2-container"));
+root.render(<App />);
