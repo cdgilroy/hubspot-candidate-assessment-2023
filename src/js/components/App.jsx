215f872519a68c2604a3cbee8d6457bfa61fee82
Chris Gilroy <cdgilroy24@gmail.com>
2023-05-21 16:29:22 +0100

    feat: load data from API endpoint

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
 export const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/HubSpotWebTeam/CodeExercise/main/src/js/data/data.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  console.log(data);
+
   return <h1>Hello, I&apos;m a React component!</h1>;
 };
