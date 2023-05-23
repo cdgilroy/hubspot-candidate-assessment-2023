c9047fca83206d7418166747d2faaa61f9655833
Chris Gilroy <cdgilroy24@gmail.com>
2023-05-23 12:53:19 +0100

    feat: style header bar a little bit

--- src/js/components/HeaderBar.jsx
+++ src/js/components/HeaderBar.jsx
@@ -11,27 +11,31 @@ export const HeaderBar = ({
 }) => {
   return (
     <div>
-      <div>
+      <div className="top-row">
         <MultiSelect
+          className="multi-select"
           options={genres}
           value={selectedGenres}
           onChange={setSelectedGenres}
           labelledBy="Select Genres"
         />
         <MultiSelect
+          className="multi-select"
           options={years}
           value={selectedYears}
           onChange={setSelectedYears}
           labelledBy="Select Years"
         />
-        <input type="text" />
+        <input className="search-field" type="text" placeholder="Search..." />
       </div>
-      <div>
+      <div className="bottom-row">
         <input id="radio-movies" type="radio" />
         <label htmlFor="radio-movies">Movies</label>
         <input id="radio-books" type="radio" />
         <label htmlFor="radio-books">Books</label>
-        <button onClick={clearFiltersHandler}>Clear Filters</button>
+        <button className="clear-button" onClick={clearFiltersHandler}>
+          Clear Filters
+        </button>
       </div>
     </div>
   );
